5110c388ab4b4ddef8e4526e8d28043ea41edd31
Vera K <fogel_ver@mail.ru>
2023-12-18 12:42:33 +0300

    feat: add get comedian by id request handling

--- index.js
+++ index.js
@@ -24,32 +24,54 @@ const checkFiles = async () => {
   return true;
 }
 
+const sendData = (res, data) => {
+  res.writeHead(200, {
+    'Content-Type': 'text/json; charset=utf-8',
+    'Access-Control-Allow-Origin': '*',
+  });
+
+  res.end(data);
+}
+
+const sendError = (res, statusCode, errorMessage) => {
+  res.writeHead(statusCode, {
+    'Content-Type': 'text/plain; charset=utf-8',
+  });
+  res.end(errorMessage);
+}
+
 const startServer = async () => {
   if (!(await checkFiles())) {
     return;
   }
 
   http.createServer(async (req, res) => {
-    if (req.method === 'GET' && req.url === '/comedians') {
+    const segments = req.url.split('/').filter(Boolean);
+
+    if (req.method === 'GET' && segments[0] === 'comedians') {
       try {
         const data = await fs.readFile(COMEDIANS, 'utf-8');
 
-        res.writeHead(200, {
-          'Content-Type': 'text/json; charset=utf-8',
-          'Access-Control-Allow-Origin': '*',
-        });
-        res.end(data);
+        if (segments.length === 2) {
+          const comedian = JSON.parse(data).find((comedian => comedian.id === segments[1]));
+
+          if (!comedian) {
+            sendError(res, 404, 'Stand-up comedian not found');
+            return;
+          }
+
+          sendData(res, JSON.stringify(comedian));
+          return;
+        }
+
+        sendData(res, data);
+        return;
       } catch (error) {
-        res.writeHead(500, {
-          'Content-Type': 'text/plain; charset=utf-8',
-        });
-        res.end(`Sorry, serverside error occurred: ${error}`);
+        console.log(error);
+        sendError(res, 500, `Sorry, server-side error occurred: ${error}`);
       }
     } else {
-      res.writeHead(404, {
-        'Content-Type': 'text/plain; charset=utf-8',
-      });
-      res.end('Not found');
+      sendError(res, 404, 'Not found');
     }
   })
     .listen(PORT);
