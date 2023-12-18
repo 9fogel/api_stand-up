9c637bbec6128ba154ca864ebd754b04e289cf60
Vera K <fogel_ver@mail.ru>
2023-12-18 12:07:17 +0300

    feat: add files checker

--- index.js
+++ index.js
@@ -2,14 +2,37 @@ import http from 'node:http';
 import fs from 'node:fs/promises';
 
 const PORT = 8080;
-const COMEDIANS = './comdeians.json';
+const COMEDIANS = './comedians.json';
 const CLIENTS = './clients.json';
 
+const checkFiles = async () => {
+  try {
+    await fs.access(COMEDIANS);
+  } catch (error) {
+    console.error(`File ${COMEDIANS} not found!`)
+    return false;
+  }
+
+  try {
+    await fs.access(CLIENTS);
+  } catch (error) {
+    await fs.writeFile(CLIENTS, JSON.stringify([]));
+    console.log(`File ${CLIENTS} was created!`)
+    return false;
+  }
+
+  return true;
+}
+
 const startServer = async () => {
+  if (!(await checkFiles())) {
+    return;
+  }
+
   http.createServer(async (req, res) => {
     if (req.method === 'GET' && req.url === '/comedians') {
       try {
-        const data = await fs.readFile('comedians.json', 'utf-8');
+        const data = await fs.readFile(COMEDIANS, 'utf-8');
 
         res.writeHead(200, {
           'Content-Type': 'text/json; charset=utf-8',
